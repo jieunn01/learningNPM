5838e9e661785a44f3e685212fcdddb94216dab1
jieun <bje0825@naver.com>
2021-10-17 00:23:55 +0900

    update images

---
+++ script/components/TheContactForm.js
@@ -0,0 +1,5 @@
+export default {
+    message: "Hello from the Contact Module!",
+
+    contactMe: () => { console.log("I am a module!"); }
+}
---
+++ script/main.js
@@ -0,0 +1,9 @@
+// import go at the top of my main JS file
+import ContactForm from "./components/TheContactForm.js";
+
+(() => {
+    // stub
+    ContactForm.contactMe();
+
+    console.log(ContactForm.message); 
+})();
