694d76229c0311b80303e1256fa6dbf11ec92f1f
jieun <bje0825@naver.com>
2021-10-24 04:51:08 +0900

    finishing part week 5

--- js/main.js
+++ js/main.js
@@ -6,38 +6,38 @@
     function getData() {
 
         // use the Fetch API to retrieve some data - eg. dog fetch 
-        fetch("./data.json")
+        fetch("./includes/functions.php")
         .then(res => res.json()) //unpack the API response (trun it back into plain JS format file)
         .then(data => {
-            console.table(data); //what's inside?
+            // console.table(data); //what's inside?
 
             // do anything else here that we want with our data
             // call a function to generate our dynamic content
-            buildTeam(data);
+            buildTeam(data[0]);
         })
         .catch(error => console.error(error)); // catch and report any errors 
     }
 
     function buildTeam(info) {
-        // first, get a referece to all of the people (the keys of the info object)
-        let team = Object.keys(info);
+    
+        // debugger;
         
-        team.forEach(person => {
+        info.forEach(person => {
             let panel = theTemplate.cloneNode(true),
                 memberInfo = panel.firstElementChild.children;
 
-            memberInfo[0].querySelector('img').src = `images/${info[person].biopic}`;
-            memberInfo[1].textContent = info[person].name;
-            memberInfo[2].textContent = info[person].role;
-            memberInfo[3].textContent = info[person].nickname;
+            memberInfo[0].querySelector('img').src = `images/${person.biopic}`;
+            memberInfo[1].textContent = person.name;
+            memberInfo[2].textContent = person.role;
+            memberInfo[3].textContent = person.nickname;
 
             //put the virtual panel in the team sevtion in our HTML page
             theTeam.appendChild(panel);
         })
 
-        // debugger;
     }
 
     // invoke the getData function 
+    // OVERALL ! Fetching data, Converting it, Processing it 
     getData();
 })()
