450e71a44da981732f47edc26699408a96a2994f
jieun <bje0825@naver.com>
2021-10-24 05:11:58 +0900

    week 5 part2 js import

--- js/main.js
+++ js/main.js
@@ -1,22 +1,11 @@
+// imports always go at the top
+import { getData } from "./components/TheDataMiner.js";
+
 (() => {
     const  theTeam = document.querySelector("#team-members"),
         theTemplate = document.querySelector("#bio-template").content;
 
     //start with a fetch call
-    function getData() {
-
-        // use the Fetch API to retrieve some data - eg. dog fetch 
-        fetch("./includes/functions.php")
-        .then(res => res.json()) //unpack the API response (trun it back into plain JS format file)
-        .then(data => {
-            // console.table(data); //what's inside?
-
-            // do anything else here that we want with our data
-            // call a function to generate our dynamic content
-            buildTeam(data[0]);
-        })
-        .catch(error => console.error(error)); // catch and report any errors 
-    }
 
     function buildTeam(info) {
     
